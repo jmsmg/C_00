bdb3b9f151dd4f5919b0117110599011022039b9
Seonggon Cho <seonggch@c1r7s7.42seoul.kr>
2021-10-13 17:02:07 +0900

    ex05

--- ex05/ft_is_print_comb.c
+++ ex05/ft_is_print_comb.c
@@ -6,12 +6,34 @@
 /*   By: seonggch <seonggch@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 12:59:50 by seonggch          #+#    #+#             */
-/*   Updated: 2021/10/13 13:05:55 by seonggch         ###   ########.fr       */
+/*   Updated: 2021/10/13 16:58:40 by seonggch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
+	char	array[3];
+
+	array[0] = '0';
+	while (array[0] <= '7')
+	{
+		array[1] = array[0] + 1;
+		while (array[1] <= '8')
+		{
+			array[2] = array[1] + 1;
+			while (array[2] <= '9')
+			{
+				if (!(array[0] == '7' && array[1] == '8' && array[2] == '9'))
+				{
+					write(1, ",", 1);
+				}
+				write(1, array, 3);
+				array[2]++;
+			}
+			array[1]++;
+		}
+		array[0]++;
+	}
 }
