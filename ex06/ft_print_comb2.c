28aa5054c29fa0bc77d1dfee88f3664980f991ae
Seonggon Cho <seonggch@c1r7s7.42seoul.kr>
2021-10-16 17:39:37 +0900

    ex06

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -14,15 +14,15 @@ void	ft_print_comb2(void)
 	b = 0;
 	while (a <= 98)
 	{
-		b= a+1;
+		b = a + 1;
 		while (b <= 99)
 		{
-			ft_putchar(a/10+48);
-			ft_putchar(a%10+48);
+			ft_putchar(a / 10 + 48);
+			ft_putchar(a % 10 + 48);
 			ft_putchar(' ');
-			ft_putchar(b/10+48);
-			ft_putchar(b%10+48);
-			if(!(a == 98 && b == 99))
+			ft_putchar(b / 10 + 48);
+			ft_putchar(b % 10 + 48);
+			if (! (a == 98 && b == 99))
 			{
 				ft_putchar (',');
 			}
