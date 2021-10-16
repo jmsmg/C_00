ecc1a595f05784d48195c46cc1ca65559f8d8917
Seonggon Cho <seonggch@c1r7s7.42seoul.kr>
2021-10-16 17:06:10 +0900

    ex06 아직

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void ft_print_comb2(void)
+{
+	char a;
+	char b;
+	
+	a = 0;
+	b = 0;
+	while (a <= 98)
+	{		
+		while (b <= 99)
+		{
+			ft_putchar(a/10 + 48);
+			ft_putchar(a%10 + 48);
+			ft_putchar(' ');
+			ft_putchar(b%10 + 48);
+			ft_putchar(b%10 + 48);
+			ft_putchar(',');
+			b++;
+		}
+		a++;
+	}
+}
