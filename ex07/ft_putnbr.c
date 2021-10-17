5fea76abea2eb46b808c97197ccb39c3ccc92281
Seonggon Cho <seonggch@c3r1s3.42seoul.kr>
2021-10-17 20:22:08 +0900

    ex07

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,40 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonggch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 20:15:07 by seonggch          #+#    #+#             */
+/*   Updated: 2021/10/17 20:20:53 by seonggch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_rec(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb > 9)
-    {
-        ft_rec(nb / 10);
-        nb = nb % 10 + 48;
-        ft_putchar(nb);
-    }
-    else
-    {
-        nb = nb + 48;
-        ft_putchar(nb);
-    }
-}
-
-void    ft_putnbr(int nb)
-{
-    if (nb >= 0 && nb <= 9)
-    {
-        nb = nb + 48;
-        ft_putchar(nb);
-    }
-    else if (nb <= 2147483647 && nb > 9)
-        ft_rec(nb);
-    else if (nb == -2147483648)
-        write(1, "-2147483648", 11);
-    else
-    {
-        nb = -nb;
-        ft_putchar('-');
-        ft_rec(nb);
-    }
+	if (nb > 9)
+	{
+		ft_putchar(nb / 10);
+		ft_putchar(nb % 10);
+	}
+	else
+	{
+		ft_putnbr(nb + 48)
+	}
 }
