f832a2de4b982b8fd83ae5025c0f917a8cacb8dc
Seonggon Cho <seonggch@c3r1s2.42seoul.kr>
2021-10-25 01:43:07 +0900

    gg

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: seonggch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 22:25:27 by seonggch          #+#    #+#             */
-/*   Updated: 2021/10/24 22:45:05 by seonggch         ###   ########.fr       */
+/*   Updated: 2021/10/25 01:42:29 by seonggch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,29 @@ void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
 void	ft_print_combn(int n)
 {
 	char array[2];
 
-	array[0] = 0;
+	array[0] = '0';
 	while (array[0] <= 99)
 	{
 		array[1] = array[0] + 1;
-		while (b <= 99)
+		while (array[1] <= 99)
 		{
-			f
+			ft_putchar(48 + array[0] / 10);
+			ft_putchar(48 + array[0] % 10);
+			ft_putchar(' ');
+			ft_putchar(48 + array[1] / 10);
+			ft_putchar(48 + array[1] % 10);
+			if (array[0] + array[1] != 98 + 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			array[1]++;
 		}
+		array[0]++;
 	}
 }
